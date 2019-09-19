3de2c7d8cf3837e7c2e17595a451ddb1ce0889c0
matsuzaki <shin.matsu.p@gmail.com>
2019-09-19 15:10:38 +0900

    辞書型の練習ファイルを作成

---
+++ fruits.py
@@ -0,0 +1,13 @@
+def main():
+    fruits_dict = {
+        'りんご': 'Apple',
+        'ぶどう': 'Grape',
+        'みかん': 'Orange'
+    }
+    print(f'ぶどう={fruits_dict["ぶどう"]}')
+    print(f'みかん={fruits_dict["みかん"]}')
+    print(f'りんご={fruits_dict["りんご"]}')
+
+
+if __name__ == '__main__':
+    main()
